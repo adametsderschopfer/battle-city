e87c318548b3c6b065d2aec8f962638c611ea3d9
Vladislav Adamets <adametsderschopfer@yandex.ru>
2024-11-05 23:00:59 +0200

    feat: link glad lib and create example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
-#include <glfw/glfw3.h>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include "iostream"
 
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,11 +22,21 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+
+    if (!gladLoadGL()) {
+        std::cout << "FATAL GLAD LOAD ERROR" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL major: " << GLVersion.major;
+
+    glClearColor(0,1,0,1);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-//        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
